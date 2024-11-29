33255be4514cd126dad9d903d8ee51dab2757034
Ryan <90571282+issryan@users.noreply.github.com>
2024-11-28 21:37:59 -0500

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Search from './components/Search';
 import Watchlist from './components/Watchlist';
+import ShowDetails from './components/ShowDetails';
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
                 <Route path="/" element={<Login />} />
                 <Route path="/search" element={<Search />} />
                 <Route path="/watchlist" element={<Watchlist />} />
+                <Route path="/show/:id" element={<ShowDetails />} />
             </Routes>
         </Router>
     );
