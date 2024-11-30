754e4a613028d36cb0255f8bbebaa2234e1e2d22
Ryan <90571282+issryan@users.noreply.github.com>
2024-11-30 03:51:16 -0500

    cors update

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,15 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 
 dotenv.config();
-app.use(cors());
+
+const corsOptions = {
+    origin: ['https://bingebuddys.netlify.app', 'http://localhost:3000'], 
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true, 
+};
+
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 4000;
 
