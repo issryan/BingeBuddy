9f0ad3d5e331e29d0d9584cb9a91351e011ebcb7
Ryan <90571282+issryan@users.noreply.github.com>
2024-11-30 13:13:02 -0500

    Revert "cors update"
    This reverts commit 754e4a613028d36cb0255f8bbebaa2234e1e2d22.

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 
 dotenv.config();
-
-const corsOptions = {
-    origin: ['https://bingebuddys.netlify.app', 'http://localhost:3000'], 
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true, 
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
