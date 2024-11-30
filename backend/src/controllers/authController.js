556b909f38cb50d5b14c807b49fcc2ab7f6935a4
Ryan <90571282+issryan@users.noreply.github.com>
2024-11-29 23:44:44 -0500

    switched to bcryptjs

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const UserRepository = require('../repositories/userRepository');
 const { UserRegisterDTO, UserLoginDTO } = require('../dtos/userDTO');
--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const UserRepository = require('../repositories/userRepository');
 const WatchlistRepository = require('../repositories/watchlistRepository');
 const FollowRepository = require('../repositories/followRepository');
