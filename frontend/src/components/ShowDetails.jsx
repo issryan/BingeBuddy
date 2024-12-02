dc0b68b7f1be34714a051101436953376cf2036c
Ryan <90571282+issryan@users.noreply.github.com>
2024-12-01 22:44:07 -0500

    add show details css

--- frontend/src/components/ShowDetails.jsx
+++ frontend/src/components/ShowDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import './ShowDetails.css';
+
 
 const ShowDetails = () => {
     const { id } = useParams();
